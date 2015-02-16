be074f2452e49daa88487770e1618c7e99ed9313
gooamoko <gooamoko@rambler.ru>
2015-02-16 16:12:05 +0800

    Создание интерфейса пользователя.

---
+++ src/main/java/ru/edu/pgtk/weducation/ejb/GOSExamsEJB.java
@@ -0,0 +1,61 @@
+package ru.edu.pgtk.weducation.ejb;
+
+import java.util.List;
+import javax.ejb.EJBException;
+import javax.ejb.Stateless;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import ru.edu.pgtk.weducation.entity.GOSExam;
+import ru.edu.pgtk.weducation.entity.StudyPlan;
+import ru.edu.pgtk.weducation.entity.Subject;
+
+@Stateless
+@Named("gosexamsEJB")
+public class GOSExamsEJB {
+
+  @PersistenceContext(unitName = "weducationPU")
+  private EntityManager em;
+  
+  public GOSExam get(final int id) {
+    GOSExam result = em.find(GOSExam.class, id);
+    if (null != result) {
+      return result;
+    }
+    throw new EJBException("GOSExam not found with id " + id);
+  }
+  
+  public List<GOSExam> findByPlan(final StudyPlan plan) {
+    TypedQuery<GOSExam> q = em.createQuery(
+            "SELECT g FROM GOSExam g WHERE (g.plan = :p) ORDER BY g.subject", GOSExam.class);
+    q.setParameter("p", plan);
+    return q.getResultList();
+  }
+  
+  public GOSExam save(GOSExam item) {
+    if (item.getSubjectCode() > 0) {
+      Subject s = em.find(Subject.class, item.getSubjectCode());
+      if (null != s) {
+        item.setSubject(s);
+      } else {
+        throw new EJBException("Subject not found with id " + item.getSubjectCode());
+      }
+    } else {
+      throw new EJBException("Wrong Subject code " + item.getSubjectCode());
+    }
+    if (item.getId() == 0) {
+      em.persist(item);
+      return item;
+    } else {
+      return em.merge(item);
+    }
+  }
+  
+  public void delete(final GOSExam item) {
+    GOSExam g = em.find(GOSExam.class, item.getId());
+    if (null != g) {
+      em.remove(g);
+    }
+  }
+}
