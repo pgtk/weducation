2dd84c186b931d75d858e57704cc960285bca5e7
gooamoko <gooamoko@rambler.ru>
2015-05-13 10:13:30 +0800

    Отфильтрован список учебных планов для отделений. Отображаются только планы, соответствующие профилям. Отфильтрован список специальностей для учебных планов при редактировании их отделением.

--- src/main/java/ru/edu/pgtk/weducation/jsf/StudyPlansMB.java
+++ src/main/java/ru/edu/pgtk/weducation/jsf/StudyPlansMB.java
@@ -1,10 +1,16 @@
 package ru.edu.pgtk.weducation.jsf;
 
 import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import ru.edu.pgtk.weducation.ejb.DepartmentsEJB;
+import ru.edu.pgtk.weducation.ejb.SpecialitiesEJB;
 import ru.edu.pgtk.weducation.ejb.StudyPlansEJB;
+import ru.edu.pgtk.weducation.entity.Department;
+import ru.edu.pgtk.weducation.entity.Speciality;
 import ru.edu.pgtk.weducation.entity.StudyPlan;
 
 @ManagedBean(name = "studyPlansMB")
@@ -13,7 +19,43 @@ public class StudyPlansMB extends GenericBean<StudyPlan> implements Serializable
 
   @EJB
   private transient StudyPlansEJB ejb;
+  @EJB
+  private transient DepartmentsEJB departments;
+  @EJB
+  private transient SpecialitiesEJB specialities;
   private int planCode;
+  private List<StudyPlan> plansList;
+  private List<Speciality> specialitiesList;
+  private Department dep;
+
+  @PostConstruct
+  private void loadDepartment() {
+    try {
+      if ((user != null) && user.isDepartment()) {
+        dep = departments.get(user.getCode());
+      } else {
+        dep = null;
+      }
+    } catch (Exception e) {
+      dep = null;
+    }
+  }
+
+  private void prepareList() {
+    try {
+      if (dep != null) {
+        plansList = ejb.findByDepartment(dep);
+        specialitiesList = specialities.findByDepartment(dep);
+      } else {
+        plansList = ejb.fetchAll();
+        specialitiesList = specialities.fetchAll();
+      }
+    } catch (Exception e) {
+      // Получить отделение не удалось, выводим все планы
+      plansList = ejb.fetchAll();
+      specialitiesList = specialities.fetchAll();
+    }
+  }
 
   public int getPlanCode() {
     return planCode;
@@ -34,6 +76,20 @@ public class StudyPlansMB extends GenericBean<StudyPlan> implements Serializable
     }
   }
 
+  public List<StudyPlan> getPlans() {
+    if (plansList == null) {
+      prepareList();
+    }
+    return plansList;
+  }
+  
+  public List<Speciality> getSpecialities() {
+    if (specialitiesList == null) {
+      prepareList();
+    }
+    return specialitiesList;
+  }
+
   public void add() {
     item = new StudyPlan();
     edit = true;
@@ -43,6 +99,7 @@ public class StudyPlansMB extends GenericBean<StudyPlan> implements Serializable
     try {
       ejb.save(item);
       resetState();
+      prepareList();
     } catch (Exception e) {
       addMessage(e);
     }
@@ -54,6 +111,7 @@ public class StudyPlansMB extends GenericBean<StudyPlan> implements Serializable
         ejb.delete(item);
       }
       resetState();
+      prepareList();
     } catch (Exception e) {
       addMessage(e);
     }
