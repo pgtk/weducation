da879f9bd27e892a81969d7ca7b75608fdb3ed20
gooamoko <gooamoko@rambler.ru>
2015-05-26 17:07:54 +0800

    Разметка отчетов переведена на бланки Киржачской типографии.

--- src/main/java/ru/edu/pgtk/weducation/reports/DiplomeBlanksEJB.java
+++ src/main/java/ru/edu/pgtk/weducation/reports/DiplomeBlanksEJB.java
@@ -78,9 +78,9 @@ public class DiplomeBlanksEJB {
    * @throws DocumentException
    */
   private void prepareFonts(final int regularSize, final int smallSize)
-          throws IOException, DocumentException {
+    throws IOException, DocumentException {
     baseFont = BaseFont.createFont("fonts/times.ttf", BaseFont.IDENTITY_H,
-            BaseFont.EMBEDDED);
+      BaseFont.EMBEDDED);
     regularFont = new Font(baseFont, regularSize);
     smallFont = new Font(baseFont, smallSize);
   }
@@ -93,22 +93,23 @@ public class DiplomeBlanksEJB {
    * @throws DocumentException
    */
   private PdfPTable prepareCourseWorkTable(final List<CourseWorkMark> marks)
-          throws DocumentException {
+    throws DocumentException {
     PdfPTable table = new PdfPTable(2);
     table.setWidthPercentage(100.0f);
-    table.setWidths(new int[]{12, 2});
+    table.setWidths(new int[]{8, 2});
+//    table.setWidths(new int[]{12, 2});
     PdfPCell nameCell;
     PdfPCell markCell;
     for (CourseWorkMark mark : marks) {
       nameCell = new PdfPCell(getParagraph(mark.getSubject().getFullName()
-              + " (" + mark.getTheme() + ")",
-              smallFont, Paragraph.ALIGN_LEFT));
+        + " (" + mark.getTheme() + ")",
+        smallFont, Paragraph.ALIGN_LEFT));
       nameCell.setHorizontalAlignment(PdfPCell.ALIGN_LEFT);
 //			nameCell.setPaddingRight(getPt(3));
 //			nameCell.setLeading(0.5f, 0.6f);
       nameCell.setBorder(PdfPCell.NO_BORDER);
       markCell = new PdfPCell(getParagraph(Utils.getMarkString(mark.getMark()),
-              smallFont, Paragraph.ALIGN_CENTER));
+        smallFont, Paragraph.ALIGN_CENTER));
       markCell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
 //			markCell.setLeading(0.5f, 0.6f);
       markCell.setBorder(PdfPCell.NO_BORDER);
@@ -119,7 +120,7 @@ public class DiplomeBlanksEJB {
   }
 
   private void prepareMarkTables(final List<MarkItem> marks,
-          PdfPTable table1, PdfPTable table2) throws DocumentException {
+    PdfPTable table1, PdfPTable table2) throws DocumentException {
 
     boolean firstPage = true;
     PdfPCell nameCell;
@@ -127,7 +128,7 @@ public class DiplomeBlanksEJB {
     PdfPCell markCell;
     for (MarkItem mark : marks) {
       nameCell = new PdfPCell(getParagraph(mark.subject,
-              smallFont, Paragraph.ALIGN_LEFT));
+        smallFont, Paragraph.ALIGN_LEFT));
       nameCell.setHorizontalAlignment(PdfPCell.ALIGN_LEFT);
       nameCell.setPaddingRight(getPt(3));
       nameCell.setLeading(0.5f, 0.7f);
@@ -144,7 +145,7 @@ public class DiplomeBlanksEJB {
         table1.addCell(nameCell);
         table1.addCell(hoursCell);
         table1.addCell(markCell);
-        if (table1.calculateHeights() > 460) {
+        if (table1.calculateHeights() > getPt(152)) {
           // Опа! высота таблицы зашкаливает. Удаляем последнюю строку
           firstPage = false;
           table1.deleteLastRow();
@@ -167,7 +168,7 @@ public class DiplomeBlanksEJB {
    * @throws DocumentException
    */
   private PdfPTable prepareRenamingTable(List<Renaming> renamingList)
-          throws DocumentException {
+    throws DocumentException {
     PdfPTable table = new PdfPTable(1);
     table.setWidthPercentage(100.0f);
     PdfPCell cell;
@@ -175,13 +176,13 @@ public class DiplomeBlanksEJB {
       cell = new PdfPCell();
       cell.setBorder(PdfPCell.NO_BORDER);
       cell.addElement(getParagraph(
-              "Образовательная организация переименована в "
-              + Utils.getYear(item.getDate()) + " году;", smallFont,
-              Paragraph.ALIGN_LEFT));
+        "Образовательная организация переименована в "
+        + Utils.getYear(item.getDate()) + " году;", smallFont,
+        Paragraph.ALIGN_LEFT));
       cell.addElement(getParagraph(
-              "старое полное наименование образовательной организации: "
-              + item.getOldName(), smallFont,
-              Paragraph.ALIGN_LEFT));
+        "старое полное наименование образовательной организации: "
+        + item.getOldName(), smallFont,
+        Paragraph.ALIGN_LEFT));
       table.addCell(cell);
     }
     return table;
@@ -199,7 +200,7 @@ public class DiplomeBlanksEJB {
     try {
       prepareFonts(10, 6);
       Document document = new Document(PageSize.A4.rotate(), 15f, 15f,
-              75f, 15f);
+        75f, 15f);
       PdfWriter writer = PdfWriter.getInstance(document, stream);
       document.open();
       document.addTitle("Диплом о среднеспециальном образовании и приложение к нему.");
@@ -222,9 +223,9 @@ public class DiplomeBlanksEJB {
       String comissionDirector = card.getComissionDirector();
       String speciality = spc.getFullName();
       String learnLength = getYearString(card.getPlan().getYears()) + " "
-              + getMonthString(card.getPlan().getMonths());
+        + getMonthString(card.getPlan().getMonths());
       String oldDocument = card.getDocumentName() + ", "
-              + Utils.getYear(card.getDocumentDate()) + " год.";
+        + Utils.getYear(card.getDocumentDate()) + " год.";
       List<MarkItem> marks = new ArrayList<>();
       // Добавляем итоговые оценки
       int aload = 0;
@@ -257,7 +258,7 @@ public class DiplomeBlanksEJB {
       marks.add(new MarkItem("Государственная итоговая аттестация", card.getDiplomeLength(), 0));
       marks.add(new MarkItem("в том числе:", "", ""));
       String title = (card.isGosExam()) ? "Итоговый междисциплинарный государственный экзамен"
-              : ("Дипломный проект на тему \"" + card.getDiplomeTheme() + "\"");
+        : ("Дипломный проект на тему \"" + card.getDiplomeTheme() + "\"");
       marks.add(new MarkItem(title, "x", Utils.getMarkString(card.getDiplomeMark())));
       for (GOSMark gm : gosMarks.fetchAll(card)) {
         marks.add(new MarkItem(gm));
@@ -278,17 +279,19 @@ public class DiplomeBlanksEJB {
       // Отступ от верхнего края страницы
       firstTableCell.addElement(wrapElement(new Phrase(" ", regularFont), getPt(45)));
       // Наименование учебного заведения
-      firstTableCell.addElement(wrapElement(getParagraph(sclName, regularFont, Paragraph.ALIGN_CENTER), 188));
+//      firstTableCell.addElement(wrapElement(getParagraph(sclName, regularFont, Paragraph.ALIGN_CENTER), 188));
+      firstTableCell.addElement(wrapElement(getParagraph(sclName, regularFont, Paragraph.ALIGN_CENTER), getPt(60)));
       // Квалификация
-      firstTableCell.addElement(wrapElement(getParagraph(spc.getKvalification(), regularFont, Paragraph.ALIGN_CENTER), 97));
+//      firstTableCell.addElement(wrapElement(getParagraph(spc.getKvalification(), regularFont, Paragraph.ALIGN_CENTER), 97));
+      firstTableCell.addElement(wrapElement(getParagraph(spc.getKvalification(), regularFont, Paragraph.ALIGN_CENTER), getPt(40)));
       // Регистрационный номер
       firstTableCell.addElement(wrapElement(
-              getParagraph(card.getRegistrationNumber(), regularFont,
-                      Paragraph.ALIGN_CENTER), 40));
+        getParagraph(card.getRegistrationNumber(), regularFont,
+          Paragraph.ALIGN_CENTER), getPt(14)));
       // Дата выдачи
       firstTableCell.addElement(wrapElement(
-              getParagraph(diplomeDate, regularFont,
-                      Paragraph.ALIGN_CENTER), 40));
+        getParagraph(diplomeDate, regularFont,
+          Paragraph.ALIGN_CENTER), getPt(5)));
 
       // Добавляем первую колонку в основную таблицу. Страница диплома создана.
       mainTable.addCell(firstTableCell);
@@ -301,21 +304,21 @@ public class DiplomeBlanksEJB {
       // ФИО
       secondTableCell.addElement(wrapElement(getParagraph(psn.getFirstName(), regularFont, Paragraph.ALIGN_CENTER), getPt(5)));
       secondTableCell.addElement(wrapElement(getParagraph(psn.getMiddleName(), regularFont, Paragraph.ALIGN_CENTER), getPt(5)));
-      secondTableCell.addElement(wrapElement(getParagraph(psn.getLastName(), regularFont, Paragraph.ALIGN_CENTER), getPt(39)));
+      secondTableCell.addElement(wrapElement(getParagraph(psn.getLastName(), regularFont, Paragraph.ALIGN_CENTER), getPt(40)));
       // образовательная программа
       secondTableCell.addElement(wrapElement(getParagraph(speciality, regularFont, Paragraph.ALIGN_CENTER), getPt(30)));
       // Дата комиссии
-      secondTableCell.addElement(wrapElement(getParagraph(comissionDate, regularFont, Paragraph.ALIGN_CENTER), getPt(21)));
+      secondTableCell.addElement(wrapElement(getParagraph(comissionDate, regularFont, Paragraph.ALIGN_CENTER), getPt(25)));
       // Председатель комиссии
       secondTableCell.addElement(wrapElement(getParagraph(comissionDirector, regularFont, Paragraph.ALIGN_RIGHT), getPt(15)));
       // Руководитель организации
-      secondTableCell.addElement(wrapElement(getParagraph(schoolDirector, regularFont, Paragraph.ALIGN_RIGHT), getPt(20)));
+      secondTableCell.addElement(wrapElement(getParagraph(schoolDirector, regularFont, Paragraph.ALIGN_RIGHT), getPt(15)));
 
       mainTable.addCell(secondTableCell);
       // Добавляем подложку в виде картинки, если требуется копия выписки
       if (isCopy) {
         Image img = Image.getInstance(this.getClass().getClassLoader()
-                .getResource("images/02.jpg"));
+          .getResource("images/02.jpg"));
         img.scaleAbsolute(getPt(297), getPt(210));
         img.setAbsolutePosition(0, 0);
         document.add(img);
@@ -334,24 +337,27 @@ public class DiplomeBlanksEJB {
       // Первая колонка (страница 4 приложения к диплому)
       firstTableCell = new PdfPCell();
       firstTableCell.setPaddingLeft(15f);
-      firstTableCell.setPaddingRight(10f);
+      firstTableCell.setPaddingRight(15f);
+//      firstTableCell.setPaddingLeft(15f);
+//      firstTableCell.setPaddingRight(10f);
       firstTableCell.setBorder(PdfPCell.NO_BORDER);
 
       // Добавим таблицу курсовых в колонку
       PdfPTable courseWorkTable = wrapElement(
-              prepareCourseWorkTable(courseWorks.fetchAll(card)), 290.0f);
+        prepareCourseWorkTable(courseWorks.fetchAll(card)), getPt(105));
+      courseWorkTable.setSpacingBefore(getPt(5));
       firstTableCell.addElement(courseWorkTable);
 
       // Добавим дополнительные сведения в колонку
       PdfPTable renamingTable = wrapElement(
-              prepareRenamingTable(renamings.findByDates(card.getBeginDate(), card.getEndDate())), 80.0f);
-      renamingTable.setSpacingBefore(20.0f);
+        prepareRenamingTable(renamings.findByDates(card.getBeginDate(), card.getEndDate())), getPt(30));
+      renamingTable.setSpacingBefore(getPt(9));
       firstTableCell.addElement(renamingTable);
 
       // Добавим данные о руководителе организации
       firstTableCell.addElement(wrapElement(
-              getParagraph(schoolDirector, regularFont,
-                      Paragraph.ALIGN_RIGHT), 50.0f));
+        getParagraph(schoolDirector, regularFont,
+          Paragraph.ALIGN_RIGHT), getPt(15)));
 
       // Добавляем первую колонку в основную таблицу. Страница 4 создана.
       mainTable.addCell(firstTableCell);
@@ -360,51 +366,51 @@ public class DiplomeBlanksEJB {
       secondTableCell.setBorder(PdfPCell.NO_BORDER);
       PdfPTable secondTable = new PdfPTable(2);
       secondTable.setWidthPercentage(100.0f);
-      secondTable.setWidths(new int[]{3, 6});
-      secondTable.setSpacingBefore(20f);
+      secondTable.setWidths(new int[]{3, 7});
+//      secondTable.setWidths(new int[]{3, 6});
+      secondTable.setSpacingBefore(getPt(7));
       // Маленькая колонка (под гербом РФ)
       PdfPCell innerCell1 = new PdfPCell();
       innerCell1.setBorder(PdfPCell.NO_BORDER);
-      innerCell1.addElement(wrapElement(new Phrase("", smallFont), 100));
-      innerCell1.addElement(wrapElement(getParagraph(sclName, regularFont, Paragraph.ALIGN_CENTER), 230));
+      innerCell1.addElement(wrapElement(new Phrase("", smallFont), getPt(35)));
+      innerCell1.addElement(wrapElement(getParagraph(sclName, regularFont, Paragraph.ALIGN_CENTER), getPt(65)));
       innerCell1.addElement(wrapElement(
-              getParagraph(spo,
-                      regularFont, Paragraph.ALIGN_CENTER), 70));
+        getParagraph(spo,
+          regularFont, Paragraph.ALIGN_CENTER), getPt(32)));
       innerCell1.addElement(wrapElement(
-              getParagraph(card.getRegistrationNumber(), regularFont,
-                      Paragraph.ALIGN_CENTER), 50));
+        getParagraph(card.getRegistrationNumber(), regularFont,
+          Paragraph.ALIGN_CENTER), getPt(17)));
       innerCell1.addElement(wrapElement(
-              getParagraph(diplomeDate, regularFont,
-                      Paragraph.ALIGN_CENTER), 20));
+        getParagraph(diplomeDate, regularFont,
+          Paragraph.ALIGN_CENTER), getPt(5)));
       // Большая колонка (сведения об обладателе диплома и т.п.)
       PdfPCell innerCell2 = new PdfPCell();
       innerCell2.setBorder(PdfPCell.NO_BORDER);
-      innerCell2.addElement(wrapElement(new Phrase("", smallFont), 20));
+      innerCell2.addElement(wrapElement(new Phrase("", smallFont), getPt(7)));
       innerCell2
-              .addElement(wrapElement(
-                              getParagraph(psn.getFirstName(), regularFont,
-                                      Paragraph.ALIGN_CENTER), 50));
+        .addElement(wrapElement(
+            getParagraph(psn.getFirstName(), regularFont,
+              Paragraph.ALIGN_CENTER), getPt(16)));
       innerCell2.addElement(wrapElement(
-              getParagraph(psn.getMiddleName(), regularFont, Paragraph.ALIGN_CENTER),
-              50));
+            getParagraph(psn.getMiddleName(), regularFont,
+              Paragraph.ALIGN_CENTER), getPt(16)));
       innerCell2.addElement(wrapElement(
-              getParagraph(psn.getLastName(), regularFont,
-                      Paragraph.ALIGN_CENTER), 50));
+        getParagraph(psn.getLastName(), regularFont,
+          Paragraph.ALIGN_CENTER), getPt(17)));
       innerCell2.addElement(wrapElement(
-              getParagraph(birthDate, regularFont,
-                      Paragraph.ALIGN_CENTER), 50));
+        getParagraph(birthDate, regularFont,
+          Paragraph.ALIGN_CENTER), getPt(18)));
       innerCell2.addElement(wrapElement(
-              getParagraph(oldDocument,
-                      regularFont, Paragraph.ALIGN_LEFT), 130));
+        getParagraph(oldDocument,
+          regularFont, Paragraph.ALIGN_LEFT), getPt(40)));
       innerCell2.addElement(wrapElement(
-              getParagraph(learnLength, regularFont,
-                      Paragraph.ALIGN_CENTER), 50));
+        getParagraph(learnLength, regularFont,
+          Paragraph.ALIGN_CENTER), getPt(17)));
       innerCell2.addElement(wrapElement(
-              getParagraph(spc.getKvalification(), regularFont,
-                      Paragraph.ALIGN_CENTER), 40));
-      innerCell2
-              .addElement(wrapElement(
-                              getParagraph(speciality, regularFont, Paragraph.ALIGN_CENTER), 30));
+        getParagraph(spc.getKvalification(), regularFont,
+          Paragraph.ALIGN_CENTER), getPt(17)));
+      innerCell2.addElement(wrapElement(
+            getParagraph(speciality, regularFont, Paragraph.ALIGN_CENTER), getPt(10)));
       // Добавляем колонки
       secondTable.addCell(innerCell1);
       secondTable.addCell(innerCell2);
@@ -414,7 +420,7 @@ public class DiplomeBlanksEJB {
       // Добавляем подложку в виде картинки, если требуется копия выписки
       if (isCopy) {
         Image img = Image.getInstance(this.getClass().getClassLoader()
-                .getResource("images/03.jpg"));
+          .getResource("images/03.jpg"));
         img.scaleAbsolute(getPt(297), getPt(210));
         img.setAbsolutePosition(0, 0);
         document.add(img);
@@ -422,9 +428,9 @@ public class DiplomeBlanksEJB {
       document.add(mainTable);
       // Выведем номера страниц
       PdfContentByte canvas = writer.getDirectContent();
-      putText(canvas, regularFont, "4", 54, 16); // Страниц всего
-      putText(canvas, regularFont, "4", 27, 4); // Страница 4
-      putText(canvas, regularFont, "1", 289, 4); // Страница 1
+      putText(canvas, regularFont, "4", 55, 12); // Страниц всего
+      putText(canvas, regularFont, "4", 27, 6); // Страница 4
+      putText(canvas, regularFont, "1", 289, 6); // Страница 1
 
       // ===========================================================================
       // Сторона 1 приложения к диплому (страницы 1 и 4) сформированы. Приступаем к
@@ -437,23 +443,23 @@ public class DiplomeBlanksEJB {
       firstTableCell.setPaddingLeft(15f);
       firstTableCell.setPaddingRight(15f);
       firstTableCell.setBorder(PdfPCell.NO_BORDER);
-      firstTableCell.setMinimumHeight(300);
+      firstTableCell.setMinimumHeight(getPt(152));
       PdfPTable firstTable = new PdfPTable(3);
       firstTable.setWidthPercentage(100.0f);
       firstTable.setTotalWidth(getPt(135));
       firstTable.setWidths(new int[]{10, 2, 2});
-      firstTable.setSpacingBefore(35);
+      firstTable.setSpacingBefore(getPt(20));
       // Вторая колонка (страница 1 приложения к диплому)
       secondTableCell = new PdfPCell();
       secondTableCell.setPaddingLeft(17f);
       secondTableCell.setPaddingRight(12f);
       secondTableCell.setBorder(PdfPCell.NO_BORDER);
-      secondTableCell.setMinimumHeight(350);
+      secondTableCell.setMinimumHeight(getPt(152));
       secondTable = new PdfPTable(3);
       secondTable.setWidthPercentage(100.0f);
       firstTable.setTotalWidth(getPt(135));
       secondTable.setWidths(new int[]{10, 2, 2});
-      secondTable.setSpacingBefore(5);
+      secondTable.setSpacingBefore(getPt(8));
 
       // Заполняем данными страницы 2 и 3
       prepareMarkTables(marks, firstTable, secondTable);
@@ -464,7 +470,7 @@ public class DiplomeBlanksEJB {
       // Если требуется копия выписки, то отображаем картинку в качестве подложки
       if (isCopy) {
         Image img = Image.getInstance(this.getClass().getClassLoader()
-                .getResource("images/04.jpg"));
+          .getResource("images/04.jpg"));
         img.scaleAbsolute(getPt(297), getPt(210));
         img.setAbsolutePosition(0, 0);
         document.add(img);
@@ -472,8 +478,8 @@ public class DiplomeBlanksEJB {
       document.add(mainTable);
       // Выводим номера страниц
       canvas = writer.getDirectContent();
-      putText(canvas, regularFont, "2", 27, 4); // Страница 4
-      putText(canvas, regularFont, "3", 289, 4); // Страница 1
+      putText(canvas, regularFont, "2", 27, 6); // Страница 4
+      putText(canvas, regularFont, "3", 289, 6); // Страница 1
       // Закрываем документ
       document.close();
     } catch (IOException | DocumentException e) {
