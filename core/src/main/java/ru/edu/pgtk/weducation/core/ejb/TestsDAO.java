544128046086716b8e0e5d39b9aae1c40889b98c
voronin.leonid <voronin.leonid@pgtk.edu.ru>
2016-05-17 09:34:26 +0700

    Добавлен первый EJB для тестов
    Change-Id: I0ae7521a7c8a50d76295ecb261ca8c5287fb6d47

---
+++ core/src/main/java/ru/edu/pgtk/weducation/core/ejb/TestsDAO.java
@@ -0,0 +1,10 @@
+package ru.edu.pgtk.weducation.core.ejb;
+
+import ru.edu.pgtk.weducation.core.entity.Test;
+
+/**
+ * Created by admin on 17.05.2016.
+ */
+public interface TestsDAO extends EntityDAO<Test> {
+    // TODO Добавить сигнатуры методов
+}
