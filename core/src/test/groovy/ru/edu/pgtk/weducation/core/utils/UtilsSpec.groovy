8517054d86856646ca261d2ad6635ce06d9623ed
voronin.leonid <voronin.leonid@pgtk.edu.ru>
2016-05-27 07:18:19 +0700

    Изменен тест. Теперь он не зависит от локали.
    Change-Id: I14a0e81b953641e8f23b04cc9198438b9d54337a

--- core/src/test/groovy/ru/edu/pgtk/weducation/core/utils/UtilsSpec.groovy
+++ core/src/test/groovy/ru/edu/pgtk/weducation/core/utils/UtilsSpec.groovy
@@ -12,26 +12,31 @@ class UtilsSpec extends Specification {
 
     def "Test than prefix for float week number will be correct"() {
         expect:
-        Utils.getLenString(val) == text
+        correctString(val, text)
 
         where:
         val  | text
-        0    | "0,0 недель"
-        0.5  | "0,5 недели"
-        1    | "1,0 неделя"
-        1.5  | "1,5 недели"
-        2    | "2,0 недели"
-        2.5  | "2,5 недели"
-        3    | "3,0 недели"
-        3.5  | "3,5 недели"
-        5    | "5,0 недель"
-        6.5  | "6,5 недель"
-        15   | "15,0 недель"
-        20.5 | "20,5 недель"
-        21   | "21,0 неделя"
-        21.5 | "21,5 недели"
-        22   | "22,0 недели"
-        22.5 | "22,5 недели"
-        23   | "23,0 недели"
+        0    | "недель"
+        0.5  | "недели"
+        1    | "неделя"
+        1.5  | "недели"
+        2    | "недели"
+        2.5  | "недели"
+        3    | "недели"
+        3.5  | "недели"
+        5    | "недель"
+        6.5  | "недель"
+        15   | "недель"
+        20.5 | "недель"
+        21   | "неделя"
+        21.5 | "недели"
+        22   | "недели"
+        22.5 | "недели"
+        23   | "недели"
+    }
+
+    def boolean correctString(float value, String suffix) {
+        String sampleString = String.format("%2.1f %s", value, suffix);
+        return Utils.getLenString(value) == sampleString
     }
 }
